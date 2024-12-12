9ca66550b53e1e12873b0cbf872c59342db2337f
nrsenthilkumaran@gmail.com <nrsenthilkumaran@gmail.com>
2024-12-12 15:55:12 +0530

    commit

--- Frontend/EMS/src/Services/apiService.jsx
+++ Frontend/EMS/src/Services/apiService.jsx
@@ -4,7 +4,7 @@ const API_URL = `${import.meta.env.VITE_API_URL}/api/employee`;
 
 
 const token = localStorage.getItem('accesstoken');
-console.log('Token from localStorage:', token);
+
 
 
 if (token) {
